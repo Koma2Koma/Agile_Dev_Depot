66c425c8cfa359f639cb7d9da2c4f22aaa5c93b5
Konrad Maurer <kradjr@gmail.com>
2014-12-04 15:15:38 -0700

    iteration C1

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,5 @@ class Product < ActiveRecord::Base
 	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+	# validates :title, length: { greater_than_or_equal_to: 10 }
 end
