4d5e38303f994889997ced4e15eea0bf5b4f3df9
Konrad Maurer <kradjr@gmail.com>
2014-12-09 19:08:02 -0700

    Iteration F4... removes evidence of a cart in view if cart is empty.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui/effect-blind
 //= require jquery_ujs
-//= require jquery-ui
 //= require turbolinks
 //= require_tree .
